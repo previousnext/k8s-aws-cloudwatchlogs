6ccaae4dadec7b00d1e00426056d1eb157bf1d01
Nick Schuch <nick@myschuch.com>
2017-07-24 16:33:55 +1000

    Adds helper script for running a Docker container with labels

---
+++ workspace/scripts/docker-test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker run -l io.kubernetes.pod.namespace=$1 \
+           -l io.kubernetes.pod.name=$2 \
+           -l io.kubernetes.container.name=$3 \
+           -it busybox /bin/sh -c 'i=0; while true; do echo "$i: $(date)"; i=$((i+1)); sleep 1; done'
